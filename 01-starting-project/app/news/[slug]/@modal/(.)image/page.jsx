18b1117e4628e54c76569ad4229e9b18843429c6
lynxDev94 <lynxpounceweb@gmail.com>
2025-10-11 18:49:47 +0300

    add router back

--- 01-starting-project/app/news/[slug]/@modal/(.)image/page.jsx
+++ 01-starting-project/app/news/[slug]/@modal/(.)image/page.jsx
@@ -1,10 +1,13 @@
+"use client";
+
 import React from "react";
-import { DUMMY_NEWS } from "../../../../dummy-news.js";
-import { notFound } from "next/navigation";
+import { DUMMY_NEWS } from "../../../../../dummy-news.js";
+import { notFound, useRouter } from "next/navigation";
 
 function InterceptedImagePage({ params }) {
   const newsItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((news) => news.slug === newsItemSlug);
+  const router = useRouter();
 
   if (!newsItem) {
     notFound();
@@ -12,7 +15,7 @@ function InterceptedImagePage({ params }) {
 
   return (
     <>
-    <div className="modal-backdrop"/>
+    <div className="modal-backdrop" onClick={router.back}/>
     <dialog className="modal" open>
       <div className="fullscreen-image">
         <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
