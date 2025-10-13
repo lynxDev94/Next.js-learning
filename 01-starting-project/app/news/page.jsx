a735a81abbb4e99062f17e006a85d2f05b9ccf8c
lynxDev94 <lynxpounceweb@gmail.com>
2025-10-13 19:52:52 +0300

    data fetching in server component

--- 01-starting-project/app/news/page.jsx
+++ 01-starting-project/app/news/page.jsx
@@ -1,53 +1,20 @@
-"use client";
-
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 // import { DUMMY_NEWS } from "../../dummy-news.js";
 import NewsList from "../components/news-list.jsx";
-import { useState, useEffect } from "react";
 
-function NewsPage() {
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [news, setNews] = useState();
+async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+  
 
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-
-      if (!response.ok) {
-        setError("Failed to fetch news");
-        setIsLoading(false);
-        return;
-      }
-
-      const data = await response.json();
-      console.log(data);
-      setIsLoading(false);
-      setNews(data);
-    }
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
   }
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  const data = await response.json();
 
   return (
     <nav>
       <h1>News</h1>
-      {newsContent}
+      <NewsList news={data} />
     </nav>
   );
 }
